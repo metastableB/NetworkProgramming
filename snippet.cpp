fff87ee89dfe38e9b919e809a9837048db577fe9
metastableB <donkdennis@gmail.com>
2016-03-04 13:08:08 +0530

    totally screwed up file structure fix

--- snippet.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <chrono>
-
-int main() {
-	std::chrono::high_resolution_clock::time_point t = std::chrono::high_resolution_clock::now(); 
-	std::chrono::duration<long,std::nano> t2 (5);
-	t = t - t2;
-}
